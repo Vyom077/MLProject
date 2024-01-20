1dddcb50f168aa6521401780af21c02dbd30969e
Vyom <vyom68088@gmail.com>
2024-01-20 14:00:12 +0530

    train_test

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,20 +8,23 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join("artificts","train.csv")
-    test_data_path: str=os.path.join("artificts","test.csv")
-    raw_data_path: str=os.path.join("artifict","raw.csv")
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
+
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method")
         try:
-            df = pd.read_csv('notebooks\data\stud.csv')
+            df=pd.read_csv('notebooks\data\stud.csv')
+            
             logging.info('Read the dataset as dataFrame')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
             logging.info("Train test split initiated")
