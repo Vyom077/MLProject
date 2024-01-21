6e1ea858077bce0db50221669f1138a634318400
Vyom <vyom68088@gmail.com>
2024-01-21 12:18:44 +0530

    transformatiom

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -13,7 +13,7 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_ob_file_path=os.path.join('artifact',"preprocessor.pkl")
+    preprocessor_ob_file_path=os.path.join('artifacts',"preprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
